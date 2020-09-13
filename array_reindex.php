8d7fc55b4e8d2673a37d34c2a9cb4fd30a10f069
Wojciech Jeżowski <dirkuu@gmail.com>
2020-09-13 20:59:50 +0200

    Initial commit

---
+++ array_reindex.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+if (! function_exists('array_reindex'))
+{
+	/**
+	 * Sets new indexes for every element using values from passed column name
+	 *
+	 * @param array $array
+	 * @param mixed $columnName
+	 * @return array
+	 */
+	function array_reindex(array $array, $columnName): array {
+		return array_combine(array_column($array, $columnName), $array);
+	}
+}
